cb616a8a990982318df814071a5cccccee8b17c3
Matt Cheung <mgcheung@gmail.com>
2021-05-09 10:37:55 -0400

    Challenge 2

--- tests/test_binutils.py
+++ tests/test_binutils.py
@@ -3,8 +3,21 @@ from utils import binutils
 
 
 class TestBinUtils(unittest.TestCase):
+
     # This tests challenge 1
     def test_hex2base64(self):
         hexstring = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
         base64string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
         assert binutils.BinUtils.hex2base64(hexstring) == base64string
+
+    def test_base642hex(self):
+        hexstring = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+        base64string = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+        assert binutils.BinUtils.base642hex(base64string) == hexstring.upper()
+
+    # This tests challenge 2
+    def test_xorhexstrings(self):
+        hexstring1 = '1c0111001f010100061a024b53535009181c'
+        hexstring2 = '686974207468652062756c6c277320657965'
+        hexstring3 = '746865206b696420646f6e277420706c6179'
+        assert binutils.BinUtils.xorhexstrings(hexstring1,hexstring2) == hexstring3
--- utils/binutils.py
+++ utils/binutils.py
@@ -5,3 +5,12 @@ class BinUtils:
     @staticmethod
     def hex2base64(hexstring):
         return base64.b64encode(base64.b16decode(hexstring.upper())).decode('utf-8')
+
+    @staticmethod
+    def base642hex(b64string):
+        return base64.b16encode(base64.b64decode(b64string)).decode('utf-8')
+
+    @staticmethod
+    def xorhexstrings(hexstring1,hexstring2):
+        assert len(hexstring1) == len(hexstring2) and len(hexstring2) % 2 == 0
+        return f'{int(hexstring1,16)^int(hexstring2,16):0{len(hexstring2)}x}'
